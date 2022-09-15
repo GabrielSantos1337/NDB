9157db1ea03622e9df04a148408e5b5a5e846cb4
GabrielSantos1337 <80500861+GabrielSantos1337@users.noreply.github.com>
2022-09-15 18:58:40 -0300

    Atualização 15/09
    Estudo biblioteca python: Pandas, Plotly.express, streamlit e openpyxl

--- NY/app.py
+++ NY/app.py
@@ -4,15 +4,26 @@ import streamlit as st #pip install streamlit
 
 
 # emojis: https://www.webfx.com/tools/emoji-cheat-sheet/
-set.set_page_config(page_title="A última do Ano", page_icon=":smoking:", layout="wide")
+st.set_page_config(page_title="A última do Ano", page_icon=":smoking:", layout="wide")
+st.title('A Última do Ano')
 
 df = pd.read_excel(
     io='ano_novo.xlsx',
     engine='openpyxl',
-    sheet_name='NY',
-    skiprows=3,
-    usecols='B:R',
-    nrows=12 
+    sheet_name=0,
+    skiprows=1,
+    usecols='A,B,F,I',
+    nrows=12,
 )
 
 st.dataframe(df)
+
+
+
+# ---- SIDEBAR ----
+st.sidebar.header("Filtre aqui:")
+fellas = st.sidebar.multiselect(
+    "Os Fellas foram selecionados",
+    options=df["Fellas"].unique(),
+    default=df["Fellas"].unique()
+)
